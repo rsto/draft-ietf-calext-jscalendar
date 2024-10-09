71a0ed3319e1f67dd4ddb481030d46779ca49807
Robert Stepanek <rsto@fastmailteam.com>
2024-10-09 09:45:19 +0200

    Scaffold conversion sections

--- spec.py
+++ spec.py
@@ -10,7 +10,50 @@ def load(fname="spec.yaml"):
         return yaml.safe_load(f)
 
 
-env = Environment(loader=FileSystemLoader("xmlsrc"))
-template = env.get_template("main.xml")
-with open('draft-ietf-calext-jscalendar.xml', 'w') as f:
-    print(template.render(), file=f)
+def convertible_comp_elems(comp):
+    elems = []
+    for name, prop in comp.get("properties").items():
+        if prop and prop.get("convert"):
+            elems.append(
+                {
+                    "kind": "Property",
+                    "name": name,
+                    "prop": prop.get("convert").get("property"),
+                    "note": prop.get("convert").get("note", ""),
+                }
+            )
+    if comp.get("components"):
+        for name, subcomp in comp.get("components").items():
+            if subcomp and subcomp.get("convert"):
+                elems.append(
+                    {
+                        "kind": "Component",
+                        "name": name,
+                        "prop": subcomp.get("convert").get("property"),
+                        "note": subcomp.get("convert").get("note", ""),
+                    }
+                )
+    return elems
+
+
+def inconvertible_props(comp):
+    return [
+        name
+        for name, prop in comp.get("properties").items()
+        if not prop or not prop.get("convert")
+    ]
+
+
+def main():
+    spec = load()
+
+    env = Environment(loader=FileSystemLoader("xmlsrc"))
+    env.globals["inconvertible_props"] = inconvertible_props
+    env.globals["convertible_comp_elems"] = convertible_comp_elems
+
+    template = env.get_template("main.xml")
+    print(template.render(spec))
+
+
+if __name__ == "__main__":
+    main()
