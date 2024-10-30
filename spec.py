6abcddcec416fb8aed7faf9048ebe10455cbabf6
Robert Stepanek <rsto@fastmailteam.com>
2024-10-30 10:08:20 +0000

    Print inconvertible JSCalendar properties

--- spec.py
+++ spec.py
@@ -1,8 +1,53 @@
 import yaml
 import os
+import pprint
+
 from collections import defaultdict
 
 from jinja2 import Environment, FileSystemLoader
+
+
+def jsnoconv(spec):
+    allprops = defaultdict(set)
+    for prop_name, prop in spec["jscalendar"]["properties"].items():
+        if prop_name == "@type":
+            continue
+        for obj_name in prop["objects"]:
+            allprops[obj_name].add(prop_name)
+
+    convprops = defaultdict(set)
+    for comp_name, comp in spec["icalendar"]["components"].items():
+        if "convert" in comp:
+            for prop_name, prop in comp["properties"].items():
+                if "convert" in prop:
+                    convprops[comp["convert"]["object"]].add(
+                        prop["convert"]["property"]
+                    )
+                    for param_name, param in spec["icalendar"]["properties"][prop_name][
+                        "parameters"
+                    ].items():
+                        if "convert" in param:
+                            if "object" in param["convert"]:
+                                convprops[param["convert"]["object"]].add(
+                                    param["convert"]["property"]
+                                )
+                            else:
+                                convprops[comp["convert"]["object"]].add(
+                                    param["convert"]["property"]
+                                )
+            if "components" in comp:
+                for subcomp_name, subcomp in comp["components"].items():
+                    if "convert" in subcomp:
+                        convprops[comp["convert"]["object"]].add(
+                            subcomp["convert"]["property"]
+                        )
+
+    for obj_name, obj_props in allprops.items():
+        if not obj_name in convprops:
+            print(f"Uncovered object: {obj_name}")
+        else:
+            diff = obj_props - convprops[obj_name]
+            print(f"{obj_name}: {diff}")
 
 
 def load(fname="spec.yaml"):
@@ -10,40 +55,15 @@ def load(fname="spec.yaml"):
         return yaml.safe_load(f)
 
 
-def prop_stats(spec):
-    converted = set()
-    covered = set()
-    for comp_name, comp in spec["icalendar"]["components"].items():
-        for prop_name, prop in comp["properties"].items():
-            covered.add(prop_name)
-            if "convert" in prop:
-                converted.add(prop_name)
-    all = set(prop_name for prop_name in spec["icalendar"]["properties"])
-    return {"all": all, "converted": converted, "covered": covered}
-
-
-def conv_props(spec):
-    conv = defaultdict(lambda: defaultdict(lambda: { "comps": set(), "object": None }))
-    for comp_name, comp in spec["icalendar"]["components"].items():
-        for prop_name, prop in comp["properties"].items():
-            if "convert" in prop:
-                conv[prop_name][prop["convert"]["property"]]["comps"].add(comp_name)
-                if "object" in prop["convert"]:
-                    conv[prop_name][prop["convert"]["property"]]["object"] = prop["convert"]["object"]
-            else:
-                conv[prop_name]
-    return conv
-
-
 def main():
     spec = load()
     env = Environment(loader=FileSystemLoader("xmlsrc"))
 
-    #template = env.get_template("main.xml")
-    #print(template.render({"spec": spec, "conv_props": conv_props(spec)}))
+    # template = env.get_template("inconvertible.xml")
+    # print(template.render({"spec": spec}))
 
-    template = env.get_template("inconvertible.xml")
-    print(template.render({"spec": spec}))
+    jsnoconv(spec)
+
 
 if __name__ == "__main__":
     main()
