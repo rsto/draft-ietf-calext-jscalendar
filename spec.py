0f5c2aad3058a0d967b3e3975a51165a4862e723
Robert Stepanek <rsto@fastmailteam.com>
2024-11-01 14:56:08 +0000

    Snapshot spec.py and yaml file

--- spec.py
+++ spec.py
@@ -11,12 +11,29 @@ def load(fname="spec.yaml"):
     with open(fname, "r", encoding="utf-8") as f:
         return yaml.safe_load(f)
 
+def inconvprops(s):
+    props = []
+    for prop_name, prop in s["icalendar"]["properties"].items():
+        prop["name"] = prop_name
+        params = []
+        for param_name, param in prop["parameters"].items():
+            if not "convert" in param:
+                param = s["icalendar"]["parameters"][param_name]
+                param["name"] = param_name
+                params.append(param)
+        if len(params) > 0:
+            prop["parameters"] = params
+            props.append(prop)
+    props.sort(key=lambda p: p["name"])
+    return props
+
 
 def main():
     spec = load()
     env = Environment(loader=FileSystemLoader("xmlsrc"))
     template = env.get_template("inconvertible.xml")
-    print(template.render({"spec": spec }))
+    #pprint.pp(inconvprops(spec))
+    print(template.render({"spec": spec, "props": inconvprops(spec) }))
 
 
 if __name__ == "__main__":
